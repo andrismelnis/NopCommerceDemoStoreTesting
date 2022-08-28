21454e3251d7b9bca26565aa8c703dbe01f385a8
andrismelnis <melnis.andris87@gmail.com>
2022-08-28 16:37:13 +0300

    code update

--- test/test_search_for_laptop.py
+++ test/test_search_for_laptop.py
@@ -3,35 +3,26 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.common.action_chains import ActionChains
-import time
+from  webpage.website_actions import serch_for_laptop
+
 
 class TestSearchForLaptop:
     driver = ''
-
+    
     def setup_method(self):
         self.driver = webdriver.Chrome(ChromeDriverManager().install())
         self.driver.implicitly_wait(5)
         self.driver.get('https://demo.nopcommerce.com/')
         self.driver.maximize_window()
 
-    
+
 
     def test_search_for_laptop(self):
-        mouse_action = ActionChains(self.driver)
-        nav_menu= self.driver.find_element(By.LINK_TEXT, "Computers")
-        mouse_action.move_to_element(nav_menu).perform()
-        
-        self.driver.implicitly_wait(5)
-        self.driver.find_element(By.XPATH, "/html/body/div[6]/div[2]/ul[1]/li[1]/ul/li[2]/a").click()
-        
-        self.driver.implicitly_wait(10)
-        page_move = ActionChains(self.driver)
-        needed_laptop= self.driver.find_element(By.CSS_SELECTOR, '.button-2.product-box-add-to-cart-button')
-        page_move.click(needed_laptop).perform()
+        serch_for_laptop(self)
 
-
-        time.sleep(3)      
+        actual_text = self.driver.find_element(By.XPATH, '//*[@id="bar-notification"]/div/p/a').text
+        expected_text = 'shopping cart'
+        assert actual_text == expected_text, f"Expected text: '{expected_text}', but actual text: '{actual_text}'"      
 
 
 
